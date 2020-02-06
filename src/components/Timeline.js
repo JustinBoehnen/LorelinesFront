e0fef684f1c958987aa24d3236d711d98709b660
Isaac Medlin <isaacemendlin@gmail.com>
2020-02-05 16:35:46 -0800

    merge conflicts fixed

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -1,58 +1,4 @@
 import React from 'react'
-<<<<<<< HEAD
-import { makeStyles, Grid, Typography } from '@material-ui/core'
-
-const useStyles = makeStyles(theme => ({
-  root: {
-    flexGrow: 1
-  },
-  link: {
-    color: theme.palette.secondary.main,
-    textDecoration: 'underline'
-  }
-}))
-
-export default function Account() {
-  const classes = useStyles()
-  /*
-    function FetchComponents() {
-        return (
-            //Need route for getting objects containing all components in a timeline
-        )
-    }
-
-    const RenderComponents = e => {
-        return (
-            <Button className={classes.logoutButton} onClick={props.logout}>
-              Log Out
-            </Button>
-        )
-    }
-    */
-  return (
-    <html>
-      <body>
-        <canvas
-          id='myCanvas'
-          width='200'
-          height='100'
-          style='border:1px solid #d3d3d3;'
-        >
-          Your browser does not support the HTML5 canvas tag.
-        </canvas>
-
-        <script>
-          var c = document.getElementById("myCanvas"); var ctx =
-          c.getContext("2d"); // Create gradient var grd =
-          ctx.createLinearGradient(0,0,200,0); grd.addColorStop(0,"red");
-          grd.addColorStop(1,"white"); // Fill with gradient ctx.fillStyle =
-          grd; ctx.fillRect(10,10,150,80);
-        </script>
-      </body>
-    </html>
-  )
-}
-=======
 import { makeStyles, 
     Grid, 
     Typography, 
@@ -155,4 +101,3 @@ const sideList = side => (
     </main>
   )
 }
->>>>>>> 767306ffee3e31bfdbef478cbc37897e06541946
