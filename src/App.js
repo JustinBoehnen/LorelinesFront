fd1380156ee0b5d9149845d21974f7a7a93f6f1b
Justin Boehnen <justinboehnen@gmail.com>
2020-02-11 05:21:04 -0800

    removes uuid in new user creation

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
-import React, { useState, useEffect } from 'react'
+/** @format */
+
+import React, { useState, useEffect } from 'react';
 import {
   createMuiTheme,
   MuiThemeProvider,
   CssBaseline,
   makeStyles
-} from '@material-ui/core'
-import axios from 'axios'
-import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom'
-import './App.css'
+} from '@material-ui/core';
+import axios from 'axios';
+import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
+import './App.css';
 
-import LoginForm from './components/LoginForm'
-import Home from './components/Home'
-import RegisterForm from './components/RegisterForm'
-import RegisterConfirmation from './components/RegisterConfirmation'
-import ForgotPassword from './components/ForgotPassword'
+import LoginForm from './components/LoginForm';
+import Home from './components/Home';
+import RegisterForm from './components/RegisterForm';
+import RegisterConfirmation from './components/RegisterConfirmation';
+import ForgotPassword from './components/ForgotPassword';
 
-const uuidv4 = require('uuid/v4')
+const uuidv4 = require('uuid/v4');
 
 const light = createMuiTheme({
   palette: {
@@ -32,7 +34,7 @@ const light = createMuiTheme({
     secondary: { main: '#000' },
     error: { main: '#ff0000' }
   }
-})
+});
 
 const dark = createMuiTheme({
   palette: {
@@ -50,7 +52,7 @@ const dark = createMuiTheme({
     error: { main: '#ff0000' },
     type: 'dark'
   }
-})
+});
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -62,29 +64,29 @@ const useStyles = makeStyles(theme => ({
     alignItems: 'center',
     justifyContent: 'center'
   }
-}))
+}));
 
 export default function App() {
-  const [user, setUser] = useState({})
-  const [auth, setAuth] = useState(true)
-  const [theme, setTheme] = useState('dark')
+  const [user, setUser] = useState({});
+  const [auth, setAuth] = useState(true);
+  const [theme, setTheme] = useState('dark');
 
-  const classes = useStyles()
+  const classes = useStyles();
 
   useEffect(() => {
-    let token = localStorage.getItem('token')
+    let token = localStorage.getItem('token');
     if (token) {
       axios
         .put('https://lorelines-expressapi.herokuapp.com/api/users/token', {
           token
         })
         .then(res => {
-          localStorage.setItem('token', res.data)
-          setAuth(true)
+          localStorage.setItem('token', res.data);
+          setAuth(true);
         })
-        .catch(err => console.log(err))
+        .catch(err => console.log(err));
     }
-  })
+  });
 
   const tryLogin = async (email, password) => {
     try {
@@ -94,60 +96,57 @@ export default function App() {
           email,
           password
         }
-      )
-      localStorage.setItem('token', data)
-      setAuth(true)
-      return true
+      );
+      localStorage.setItem('token', data);
+      setAuth(true);
+      return true;
     } catch (err) {
       return false;
     }
-  }
+  };
 
   const createUser = async (name, email, password) => {
     try {
-      let id = uuidv4()
-
       const { data } = await axios.post(
         'https://lorelines-expressapi.herokuapp.com/api/users',
         {
-          id,
           name,
           email,
           password
         }
-      )
-      localStorage.setItem('token', data)
-      setAuth(true)
-      return true
+      );
+      localStorage.setItem('token', data);
+      setAuth(true);
+      return true;
     } catch (err) {
-      return false
+      return false;
     }
-  }
+  };
 
   const logout = () => {
-    localStorage.removeItem('token')
-    setAuth(false)
-  }
+    localStorage.removeItem('token');
+    setAuth(false);
+  };
 
   return (
     <MuiThemeProvider theme={theme === 'dark' ? dark : light}>
       <CssBaseline />
       <Router>
         <div className={classes.root}>
-          {auth && <Redirect to='/app' />}
-          <Route path='/app'>
+          {auth && <Redirect to="/app" />}
+          <Route path="/app">
             <Home logout={logout} auth={auth} />
           </Route>
-          <Route exact path='/'>
+          <Route exact path="/">
             <LoginForm className={classes.center} tryLogin={tryLogin} />
           </Route>
-          <Route path='/forgot' component={ForgotPassword} />
-          <Route exact path='/register'>
+          <Route path="/forgot" component={ForgotPassword} />
+          <Route exact path="/register">
             <RegisterForm createUser={createUser} />
           </Route>
-          <Route path='/register/confirm' component={RegisterConfirmation} />
+          <Route path="/register/confirm" component={RegisterConfirmation} />
         </div>
       </Router>
     </MuiThemeProvider>
-  )
+  );
 }
