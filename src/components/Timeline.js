db933f47c5761758ae26d5c330ab4b7ace5ecfde
Isaac Medlin <isaacemendlin@gmail.com>
2020-02-12 15:34:55 -0800

    Timeline sidebar fized ... again

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -4,7 +4,6 @@ import {
   Grid,
   Typography,
   Drawer,
-  Button,
   List,
   Divider,
   ListItem,
@@ -17,69 +16,31 @@ import FormatListNumbered from '@material-ui/icons/FormatListNumberedOutlined'
 import FlightTakeoffOutlined from '@material-ui/icons/FlightTakeoffOutlined'
 import CallMade from '@material-ui/icons/CallMadeOutlined'
 
-const drawerWidthExpanded = 240
-const drawerWidthCondensed = 58
+const drawerWidth = 120
 
-const useStyles = makeStyles({
-  list: {
-    width: 250
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: 'flex',
   },
-  fullList: {
-    width: 'auto'
-  }
-})
+  appBar: {
+    zIndex: theme.zIndex.drawer + 1,
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+  },
+  drawerPaper: {
+    width: drawerWidth,
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(3),
+  },
+  toolbar: theme.mixins.toolbar,
+}));
+
 export default function Timeline() {
-  const classes = useStyles()
-  const [state, setState] = React.useState({
-    right: false
-  })
-
-  const toggleDrawer = (side, open) => event => {
-    if (
-      event.type === 'keydown' &&
-      (event.key === 'Tab' || event.key === 'Shift')
-    ) {
-      return
-    }
-
-    setState({ ...state, [side]: open })
-  }
-
-  const sideList = side => (
-    <div
-      className={classes.list}
-      role='presentation'
-      onClick={toggleDrawer(side, false)}
-      onKeyDown={toggleDrawer(side, false)}
-    >
-      <List>
-        <ListItem button key='Event Node'>
-          <ListItemIcon>
-            <FontDownloadIcon color='secondary' />
-          </ListItemIcon>
-          <ListItemText primary='Event Node' />
-        </ListItem>
-        <ListItem button key='Branching Event Node'>
-          <ListItemIcon>
-            <FormatListNumbered color='secondary' />
-          </ListItemIcon>
-          <ListItemText primary='Branching Event Node' />
-        </ListItem>
-        <ListItem button key='Teather'>
-          <ListItemIcon>
-            <CallMade color='secondary' />
-          </ListItemIcon>
-          <ListItemText primary='Teather' />
-        </ListItem>
-        <ListItem button key='Warp Node'>
-          <ListItemIcon>
-            <FlightTakeoffOutlined color='secondary' />
-          </ListItemIcon>
-          <ListItemText primary='Warp Node' />
-        </ListItem>
-      </List>
-    </div>
-  )
+  const classes = useStyles();
 
   return (
     <main className={classes.root}>
@@ -94,22 +55,6 @@ export default function Timeline() {
           <Typography>Canvas and Drag/Drop Components in progress</Typography>
         </Grid>
       </Grid>
-<<<<<<< HEAD
-
-      <div>
-        <Button onClick={toggleDrawer('right', true)}>Open Components</Button>
-        <Drawer
-          anchor='right'
-          open={state.right}
-          onClose={toggleDrawer('right', false)}
-        >
-          {sideList('right')}
-        </Drawer>
-      </div>
-    </main>
-  )
-}
-=======
       <div>
         <Drawer
           className={classes.drawer}
@@ -132,11 +77,11 @@ export default function Timeline() {
               </ListItemIcon>
               <ListItemText primary='Branching Node' />
             </ListItem>
-            <ListItem button key='Teather'>
+            <ListItem button key='Tether'>
               <ListItemIcon>
                 <CallMade color='secondary' />
               </ListItemIcon>
-              <ListItemText primary='Teather' />
+              <ListItemText primary='Tether' />
             </ListItem>
             <Divider />
             <ListItem button key='Warp Node'>
@@ -151,4 +96,3 @@ export default function Timeline() {
     </main>
   );
 }
->>>>>>> 3e77d291c39fac523f73ea4b3615c0c82f428958
