b9a20f2c9539b654c7afeda4217c903ab846bb43
Evan-D-Clark <evan.clark@oit.edu>
2020-02-18 20:01:09 -0800

    Still trying to display return of get request

--- src/components/CreateInstance.js
+++ src/components/CreateInstance.js
@@ -12,16 +12,20 @@ const useStyles = makeStyles(theme => ({
   }
 }));
 
-export default function CreateInstance() {
-  const classes = useStyles();
-  //this gets a specific custom entity, later it will get one designated by other code
-  let data = axios
+async function getEntity() {
+  return await axios
     .get(
       "http://localhost:8080/api/lorelines/5e44c8b56a5d003218847a9f/entities/5e44cf2f6a5d003218847aa1"
     )
     .then(response => {
-      response = response.data;
-      return response;
+      return response.data;
     });
-  return <main>{JSON.stringify(data)}</main>;
+}
+
+export default async function CreateInstance() {
+  const classes = useStyles();
+  //this gets a specific custom entity, later it will get one designated by other code
+  let entity = await getEntity();
+  var display = JSON.stringify(entity);
+  return <main>{display}</main>;
 }
