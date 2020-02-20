dfc0dc543d74126a96f6482bc637e021af595f7f
Evan-D-Clark <evan.clark@oit.edu>
2020-02-19 23:11:42 -0800

    Added submit button and temp header

--- src/components/CreateInstance.js
+++ src/components/CreateInstance.js
@@ -1,6 +1,6 @@
 import React from "react";
 import axios from "axios";
-import { makeStyles, Grid, Typography } from "@material-ui/core";
+import { makeStyles, Grid, Typography, TextField } from "@material-ui/core";
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -20,7 +20,7 @@ export default class CreateInstance extends React.Component {
   async componentDidMount() {
     axios
       .get(
-        "http://localhost:8080/api/lorelines/5e44c8b56a5d003218847a9f/entities/5e44cf2f6a5d003218847aa1"
+        "http://localhost:8080/api/lorelines/5e44c8b56a5d003218847a9f/entities/5e44cf2f6a5d003218847aa1/content"
       )
       .then(response => {
         const fields = response.data;
@@ -29,6 +29,19 @@ export default class CreateInstance extends React.Component {
   }
 
   render() {
-    return <ul>{JSON.stringify(this.state.fields)}</ul>;
+    return (
+      <ul>
+        <Typography>Create new instance</Typography>
+        {this.state.fields.map(function(field, index) {
+          return (
+            <li key={index}>
+              <Typography> {field.name} </Typography>
+              <TextField id="outlined-basic" varient="outlined" />
+            </li>
+          );
+        })}
+        <button>Submit</button>
+      </ul>
+    );
   }
 }
