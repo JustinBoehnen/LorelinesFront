2947d98427ce49c1f2bf71e79d52c4c80ae4fbef
Evan-D-Clark <evan.clark@oit.edu>
2020-02-22 17:42:13 -0800

    Added name to instantiation and started posting work

--- src/components/CreateInstance.js
+++ src/components/CreateInstance.js
@@ -20,8 +20,15 @@ const useStyles = makeStyles(theme => ({
 
 export default class CreateInstance extends React.Component {
   state = {
-    fields: []
+    fields: [],
+    name: []
   };
+
+  SaveInstance(event) {
+    const e = event.nativeEvent;
+    console.log(e.target[0].value);
+    console.log(e.target[1].value);
+  }
 
   async componentDidMount() {
     axios
@@ -32,31 +39,33 @@ export default class CreateInstance extends React.Component {
         const fields = response.data;
         this.setState({ fields });
       });
-  }
-
-  const onSubmit = async e => {
-    console.log("here");
+    axios
+      .get(
+        "http://localhost:8080/api/lorelines/5e44c8b56a5d003218847a9f/entities/5e44cf2f6a5d003218847aa1/name"
+      )
+      .then(response => {
+        const name = response.data;
+        this.setState({ name });
+      });
   }
 
   render() {
     return (
       <ul>
-        <Typography>Create new instance</Typography>
-        {this.state.fields.map(function(field, index) {
-          return (
-            <li key={index}>
-              <Typography> {field.name} </Typography>
-              <TextField id="outlined-basic" varient="outlined" />
-            </li>
-          );
-        })}
-        <Button
-          type="submit"
-          varient="contained"
-          onClick={async e => await onSubmit(e)}
-        >
-          Submit
-        </Button>
+        <Typography>{this.state.name}</Typography>
+        <form onSubmit={this.SaveInstance}>
+          <Typography>name</Typography>
+          <input id="outlined-basic" varient="outlined" />
+          {this.state.fields.map(function(field, index) {
+            return (
+              <li key={index}>
+                <Typography> {field.name} </Typography>
+                <input id="outlined-basic" varient="outlined" />
+              </li>
+            );
+          })}
+          <Button type="submit">Submit</Button>
+        </form>
       </ul>
     );
   }
