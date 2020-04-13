aca19dcbf89539e56a319955f99944ef3d044b1e
Isaac <isaac.medlin@oit,edu>
2020-04-12 21:28:56 -0700

    all files needed added and initialized

---
+++ src/components/PasswordRecovery/SecurityQ.js
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import axios from "axios";
+import { Grid, Typography, TextField, Button, Input } from "@material-ui/core";
+
+
+export default connect(
+  mapStateToProps,
+  matchDispatchToProps
+)(function securityQ(props) {
+
+  const [newPassword, setNewPassword] = React.useState("");
+  const [confirmPassword, setConfirmPassword] = React.useState("");
+
+  return (
+      <main className={classes.root}>
+          
+      </main>
+  )
+})
+
+
+function mapStateToProps(state) {
+	return {
+		user: state.user,
+		loreline: state.lorelineId,
+		lorelineArray: state.lorelineArray,
+	}
+}
+
+function matchDispatchToProps(dispatch) {
+	return bindActionCreators(
+		{
+			setLoreline: setLoreline,
+			setLoading: setLoading,
+			setLorelineArray: setLorelineArray,
+		},
+		dispatch
+	)
+}
