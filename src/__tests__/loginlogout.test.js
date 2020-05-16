1ad3cda7ffc4a39c5df627fc3973b18aa76aa849
Evan-D-Clark <evan.clark@oit.edu>
2020-05-15 19:49:46 -0700

    Save

--- src/__tests__/loginlogout.test.js
+++ src/__tests__/loginlogout.test.js
@@ -22,6 +22,22 @@ it("logs in and out with dummy user", () => {
 
   input = driver.activeWindow.getElement('[dataTestId="loginEmail"]');
   assert.equal(input.getAttribute("value"), "");
+});
 
-  driver.dispose();
+it("logs in and out with dummy user", () => {
+  driver.activeWindow.navigateTo("/");
+  var input = driver.activeWindow.getElement('[dataTestId="loginEmail"]');
+  input.sendKeys("FakeEmail@email.com");
+
+  input = driver.activeWindow.getElement('[dataTestId="loginPassword"]');
+  input.sendKeys("end2endTesting");
+
+  input = driver.activeWindow.getElement('[dataTestId="loginSubmit"]');
+  input.mouse.click();
+
+  input = driver.activeWindow.getElement('[dataTestId="logout"]');
+  input.mouse.click();
+
+  input = driver.activeWindow.getElement('[dataTestId="loginEmail"]');
+  assert.equal(input.getAttribute("value"), "");
 });
