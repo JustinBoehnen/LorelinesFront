35d839e805f73f7b78f724c99742a3696c9015c3
Evan-D-Clark <evan.clark@oit.edu>
2020-06-01 17:13:00 -0700

    Merge complete from polish branch

--- src/components/directory_interaction/DirectoryList.js
+++ src/components/directory_interaction/DirectoryList.js
@@ -93,88 +93,131 @@ class DirectoryList extends Component {
 
   render() {
     return (
-      <List style={{ width: 300, marginBottom: 76 }}>
-        {Array.from(this.props.directory).map((entity) => {
-          const open = this.state[entity._id] || false;
-          return (
-            <div
-              style={{
-                borderStyle: "solid",
-                borderWidth: "1px 1px 1px 3px", //t r b l
-                borderColor: entity.color,
-                marginBottom: 5,
-                borderRadius: 5,
-              }}
-              key={entity._id}
-            >
-              <ListItem style={{ width: "100%", padding: 0 }}>
-                <ButtonGroup
-                  fullWidth
-                  variant="text"
-                  aria-label="text primary button group"
-                >
-                  <Button
-                    style={{ width: 44, borderRadius: 0, color: "#777" }}
-                    onClick={() => this.handleEntityInstantiation(entity._id)}
+      <Grid>
+        <List style={{ width: 300, marginBottom: 76 }}>
+          {Array.from(this.props.directory).map((entity) => {
+            const open = this.state[entity._id] || false;
+            return (
+              <div
+                style={{
+                  borderStyle: "solid",
+                  borderWidth: "1px 1px 1px 3px", //t r b l
+                  borderColor: entity.color,
+                  marginBottom: 5,
+                  borderRadius: 5,
+                }}
+                key={entity._id}
+              >
+                <ListItem style={{ width: "100%", padding: 0 }}>
+                  <ButtonGroup
+                    fullWidth
+                    variant="text"
+                    aria-label="text primary button group"
                   >
-                    <AddCircle />
-                  </Button>
-                  <Button
-                    style={{ borderRadius: 0 }}
-                    onClick={() => this.handleEntityDropdown(entity._id)}
-                  >
-                    <Typography
-                      style={{
-                        textAlign: "left",
-                        overflow: "hidden",
-                        textOverflow: "clip",
-                        width: 272,
-                        textTransform: "none",
-                        color: entity.color,
-                      }}
-                      variant="h6"
+                    <Button
+                      style={{ width: 44, borderRadius: 0, color: "#777" }}
+                      onClick={() => this.handleEntityInstantiation(entity._id)}
                     >
-                      {entity.name}
-                    </Typography>
-                    {open ? <ExpandLess /> : <ExpandMore />}
-                  </Button>
-                  <Button
-                    style={{
-                      width: 44,
-                      borderRadius: 0,
-                      color: "#777",
-                    }}
-                    onClick={(event) =>
-                      this.handleEntityDelete(event, entity._id, entity.name)
-                    }
-                  >
-                    <Delete />
-                  </Button>
-                </ButtonGroup>
-              </ListItem>
-              <Collapse in={open} timeout="auto" unmountOnExit>
-                <List component="div" disablePadding>
-                  {entity.instances.map((instance, i) => {
-                    return (
-                      <div key={instance._id + i}>
-                        <Divider />
-                        <ListItem
-                          button
-                          onClick={() => {
-                            this.handleInstanceSelect(instance._id, entity._id);
-                          }}
-                        >
-                          <Typography>{instance.name}</Typography>
-                        </ListItem>
-                      </div>
-                    );
-                  })}
-                </List>
-              </Collapse>
-            </div>
-          );
-        })}
-      </List>
+                      <AddCircle />
+                    </Button>
+                    <Button
+                      style={{ borderRadius: 0 }}
+                      onClick={() => this.handleEntityDropdown(entity._id)}
+                    >
+                      <Typography
+                        style={{
+                          textAlign: "left",
+                          overflow: "hidden",
+                          textOverflow: "clip",
+                          width: 272,
+                          textTransform: "none",
+                          color: entity.color,
+                        }}
+                        variant="h6"
+                      >
+                        {entity.name}
+                      </Typography>
+                      {open ? <ExpandLess /> : <ExpandMore />}
+                    </Button>
+                    <Button
+                      style={{
+                        width: 44,
+                        borderRadius: 0,
+                        color: "#777",
+                      }}
+                      onClick={(event) =>
+                        this.handleEntityDelete(event, entity._id, entity.name)
+                      }
+                    >
+                      <Delete />
+                    </Button>
+                  </ButtonGroup>
+                </ListItem>
+                <Collapse in={open} timeout="auto" unmountOnExit>
+                  <List component="div" disablePadding>
+                    {entity.instances.map((instance, i) => {
+                      return (
+                        <div key={instance._id + i}>
+                          <Divider />
+                          <ListItem
+                            button
+                            onClick={() => {
+                              this.handleInstanceSelect(
+                                instance._id,
+                                entity._id
+                              );
+                            }}
+                          >
+                            <Typography>{instance.name}</Typography>
+                          </ListItem>
+                        </div>
+                      );
+                    })}
+                  </List>
+                </Collapse>
+              </div>
+            );
+          })}
+        </List>
+        <Popover
+          anchorEl={this.state.anchorEl}
+          open={Boolean(this.state.anchorEl)}
+          id={Boolean(this.state.anchorEl) ? "simple-popover" : undefined}
+          onClose={this.handleEntityDeleteCancel}
+          anchorOrigin={{
+            vertical: "bottom",
+            horizontal: "center",
+          }}
+          transformOrigin={{
+            vertical: "top",
+            horizontal: "center",
+          }}
+        >
+          <Grid
+            container
+            spacing={0}
+            align="center"
+            justify="center"
+            direction="column"
+          >
+            <Grid item>
+              <Typography variant="h6">
+                Are you sure you want to delete {this.state.deleteEntityName}?
+              </Typography>
+            </Grid>
+            <Grid item>
+              <ButtonGroup>
+                <Button color="inherit" onClick={this.deleteEntityFromDB}>
+                  Delete
+                </Button>
+                <Button color="primary" onClick={this.handleEntityDeleteCancel}>
+                  Cancel
+                </Button>
+              </ButtonGroup>
+            </Grid>
+          </Grid>
+        </Popover>
+      </Grid>
     );
   }
 }
